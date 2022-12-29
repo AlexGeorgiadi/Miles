0b8baa3c1fa52a5f52e1bddf9919544ef0af84ea
Alextos <alex-georgiadi@ya.ru>
2022-12-29 21:08:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15_000;
+        int bonus = 20;
+
+        int miles = amount / bonus;
+
+        System.out.println("Бонусные мили:");
+        System.out.println(miles);
+    }
+}
